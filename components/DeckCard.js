118c787b9f7f88bc5a4bbcdc9bc1c7612691824b
StarkTheGnr <lordsomy@hotmail.com>
2021-10-12 06:05:18 +0200

    update 3

--- components/DeckCard.js
+++ components/DeckCard.js
@@ -1,20 +1,35 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, Animated } from "react-native";
 import { connect } from 'react-redux'
+
+const AnimatedTouchable = Animated.createAnimatedComponent(TouchableOpacity)
 
 class DeckCard extends Component
 {
+	state = {
+		scale: new Animated.Value(1)
+	}
+
 	handlePress = (e) => {
+		const { scale } = this.state
+
+		Animated.sequence([
+			Animated.timing(scale, {duration: 100, toValue: 1.3, useNativeDriver: true}),
+			Animated.spring(scale, {toValue: 1, friction: 4, useNativeDriver: true})
+		]).start()
+
 		this.props.navigation.navigate("DeckInfo", { deckKey: this.props.deck.key })
 	}
 
 	render()
 	{
+		const { scale } = this.state
+
 		return (
-			<TouchableOpacity style={styles.mainview} onPress={this.handlePress}>
+			<AnimatedTouchable style={[styles.mainview, {transform: [{scale: scale}]}]} onPress={this.handlePress}>
 				<Text style={styles.decktext}>{this.props.deck.title}</Text>
 				<Text>{this.props.deck.questions.length} cards</Text>
-			</TouchableOpacity>
+			</AnimatedTouchable>
 		)
 	}
 }
